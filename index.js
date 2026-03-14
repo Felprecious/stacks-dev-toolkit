d41b2339dada716730301d537e176c3f7bc66169
Oluwaseyifunmi <134746406+Felprecious@users.noreply.github.com>
2026-03-14 09:39:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-import * as PortfolioSDK from "stx-portfolio-sdk"
+import {
+  getBalances,
+  getDeFiPositions,
+  getNFTs,
+  getPortfolioValue
+} from "stx-portfolio-sdk"
 
-console.log(PortfolioSDK)
+async function main() {
+  const address = "SP000000000000000000002Q6VF78"
+
+  const balances = await getBalances(address)
+  const defi = await getDeFiPositions(address)
+  const nfts = await getNFTs(address)
+  const value = await getPortfolioValue(address)
+
+  console.log("Balances:", balances)
+  console.log("DeFi:", defi)
+  console.log("NFTs:", nfts)
+  console.log("Portfolio Value:", value)
+}
+
+main()

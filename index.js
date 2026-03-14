f897759d3a1d2b147448fedd68386f057c2e4756
Oluwaseyifunmi <134746406+Felprecious@users.noreply.github.com>
2026-03-14 09:37:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-import { getPortfolio } from "stx-portfolio-sdk"
+import * as PortfolioSDK from "stx-portfolio-sdk"
 
-async function main() {
-  const address = "SP000000000000000000002Q6VF78"
-  const portfolio = await getPortfolio(address)
-
-  console.log("Portfolio:", portfolio)
-}
-
-main()
+console.log(PortfolioSDK)
